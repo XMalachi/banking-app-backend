fd27e521cf3ad4d3ec7c78cd356be9c60fb1a113
xmalachi <angelmalachi@gmail.com>
2023-01-22 08:21:46 +0100

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,16 +4,16 @@ const {protect, authorizeRoles} = require('../middleware/auth.js');
 const {registerUser, loginUser, logout, getUsers, getUser,updateUser,updatePassword, logInStatus, getAccountDetails} = require('../controllers/userController.js')
 
 // router.route('/').get(registerUser)
-router.get('/get-users',protect,authorizeRoles("admin"), getUsers)
-router.get('/get-user',protect, getUser)
+router.get('/getusers',protect,authorizeRoles("admin"), getUsers)
+router.get('/getuser',protect, getUser)
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.get('/logout', logout)
 
 router.get('/loggedin',logInStatus)
-router.patch('/update-user', protect, authorizeRoles("admin"), updateUser)
-router.patch('/update-password', protect, updatePassword)
-router.get('/get-account-details', protect, getAccountDetails)
+router.patch('/updateuser', protect, authorizeRoles("admin"), updateUser)
+router.patch('/updatepassword', protect, updatePassword)
+router.get('/getaccountdetails', protect, getAccountDetails)
 
 
 module.exports = router
