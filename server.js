92f018a0f4064689dcef8ecae6ca04c6b1d73d42
xmalachi <angelmalachi@gmail.com>
2023-01-23 04:29:27 +0100

    update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cookieParser())
 app.use(express.json({limit:'50mb'}))
 app.use(
     cors({
-      origin: ["http://localhost:3000", "https://eastdalefinance.onrender.com"],
+      origin: ["http://localhost:3000", "https://eastdalefinance-api.onrender.com"],
       credentials: true,
     })
   );
@@ -44,7 +44,7 @@ app.use('/api/transactions', transactionRoutes)
 const PORT =  process.env.PORT || 5000
 app.get('/', (req, res) => {
     res.send("Welcome Page")
-    console.log(req.cookies, "request cookies")
+    console.log(PORT, "port  for you")
 })
 
 
