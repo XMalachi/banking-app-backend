fbc73b3cd0bf8473552f4bbb4fe8b9d262ba3271
xmalachi <angelmalachi@gmail.com>
2023-01-23 04:52:51 +0100

    update

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -4,8 +4,8 @@ const {protect, authorizeRoles} = require('../middleware/auth.js');
 const { debitTransaction, getTransactions, getUserTransactions } = require('../controllers/transactionController.js')
 
 router.post('/debit',protect, debitTransaction)
-router.get('/get-transactions',protect, authorizeRoles("admin"), getTransactions)
-router.get('/get-user-transactions',protect, getUserTransactions)
+router.get('/gettransactions',protect, authorizeRoles("admin"), getTransactions)
+router.get('/getusertransactions',protect, getUserTransactions)
 
 
 
