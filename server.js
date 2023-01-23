9065a411bebd65713baef9a350938c07905e8372
xmalachi <angelmalachi@gmail.com>
2023-01-23 04:44:59 +0100

    update

--- server.js
+++ server.js
@@ -15,12 +15,14 @@ const app = express()
 app.use(cookieParser())
 // middlewares
 app.use(express.json({limit:'50mb'}))
-app.use(
-    cors({
-      origin: ["http://localhost:3000", "https://eastdalefinance-api.onrender.com"],
-      credentials: true,
-    })
-  );
+// app.use(
+//     cors({
+//       origin: ["http://localhost:3000", "https://eastdalefinance.onrender.com"],
+//       credentials: true,
+//     })
+//   );
+
+app.use(cors())
 app.use(express.urlencoded({extended:  false}))
 app.use(express.static(path.join(__dirname, 'build')));
 
